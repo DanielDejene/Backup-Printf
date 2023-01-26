24611379a12b3497502c4a7352b0122985a6627d
Daniel Dejene <101667125+DanielDejene@users.noreply.github.com>
2023-01-27 02:27:51 +0300

    Update README.d

--- printf-master/README.d
+++ printf-master/README.d
@@ -1,2 +1,2 @@
 This is a backup for ALX "0x11. C - printf" group project which I have done with collaborate with "Hafsa Hussein"
-( https://github.com/hedishafi/Hafsa.git }
+( https://github.com/hedishafi.git }
